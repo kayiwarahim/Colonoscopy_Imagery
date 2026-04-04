cd204f4daa2a49591899f187c8e5414d1b6bce12
kayiwarahim <kayiwarahim@gmail.com>
2026-04-05 00:19:53 +0300

    updated display names for colon labels in predict.py

--- api/predict.py
+++ api/predict.py
@@ -22,7 +22,7 @@ COLON_LABELS  = ["colon_aca", "colon_n"]
 GI_LABELS     = ["0_normal", "1_ulcerative_colitis", "2_polyps"]
 
 # Display names shown to the user
-COLON_DISPLAY = ["Normal","Adenocarcinoma"]
+COLON_DISPLAY = ["Colon Adenocarcinoma", "Colon Normal"]
 #COLON_DISPLAY = ["Adenocarcinoma", "Normal"]
 GI_DISPLAY    = ["Normal", "Ulcerative Colitis", "Polyps"]
 
